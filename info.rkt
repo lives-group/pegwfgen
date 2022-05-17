61520a55e9eb0ce0c3d533875689304229d889b1
Sam Tobin-Hochstadt <samth@indiana.edu>
2022-05-17 10:22:51 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,10 @@
 #lang info
 (define collection "peg-gen")
-(define deps '("base" "rackcheck" "rackunit"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("algorithms"
+               "base" "rackcheck" "rackunit"))
+(define build-deps '("cover-lib"
+                     "typed-peg"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/peg-gen.scrbl" ())))
 (define pkg-desc "Type safe (and thus well formed) random peg generator")
 (define version "0.0")
