b3433549a435f692b6a774b2baafeeb985d40a31
Elton M. C <eltonm.cardoso@gmail.com>
2023-08-24 13:33:57 -0300

    license

--- info.rkt
+++ info.rkt
@@ -8,4 +8,5 @@
 (define scribblings '(("scribblings/peg-gen.scrbl" ())))
 (define pkg-desc "Type safe (and thus well formed) random peg generator")
 (define version "0.1")
+(define license "MIT")
 (define pkg-authors '(EltonMC RodrigoGR LeonardoVSR ))
